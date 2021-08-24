3da59aa176cb1f3150db04a245f87329c90b0d9c
matheus.teixeira7 <matheus.teixeira7@gmail.com>
2021-08-24 12:23:54 -0300

    Primeiro commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Home = (): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <div />
+    </>
+  );
+};
+
+export default Home;
